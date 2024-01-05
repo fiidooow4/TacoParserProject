d2d2bcbe43c5757ecb28565d900307d52e37ff96
ilyas Osman <ilyaso.fiiidooow@gmail.com>
2024-01-05 10:24:27 -0600

    new

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -10,6 +10,8 @@ namespace LoggingKata
         static readonly ILog logger = new TacoLogger();
         const string csvPath = "TacoBell-US-AL.csv";
 
+        public static object corA { get; private set; }
+
         static void Main(string[] args)
         {
             // Objective: Find the two Taco Bells that are the farthest apart from one another.
@@ -30,23 +32,48 @@ namespace LoggingKata
             // Use the Select LINQ method to parse every line in lines collection
             var locations = lines.Select(parser.Parse).ToArray();
 
-  
+
             // Complete the Parse method in TacoParser class first and then START BELOW ----------
 
             // TODO: Create two `ITrackable` variables with initial values of `null`. 
             // These will be used to store your two Taco Bells that are the farthest from each other.
-            
-            // TODO: Create a `double` variable to store the distance
+            ITrackable tacoBallOne = null;
+            ITrackable tacoballTwo = null;
 
+
+            // TODO: Create a `double` variable to store the distance
+            double distance = 0;
             // TODO: Add the Geolocation library to enable location comparisons: using GeoCoordinatePortable;
             // Look up what methods you have access to within this library.
 
             // NESTED LOOPS SECTION----------------------------
-            
+
             // FIRST FOR LOOP -
             // TODO: Create a loop to go through each item in your collection of locations.
             // This loop will let you select one location at a time to act as the "starting point" or "origin" location.
             // Naming suggestion for variable: `locA`
+            for (int i = 0; i < locations.Length; i++)
+            {
+                var locA = locations[i];
+                var corA = new GeoCoordinate(locA.Location.Latitude, locA.Location.Longitude);
+
+
+                for (int j = 0; j < locations.Length; j++)
+                {
+                    var locB = locations[j];
+                    var corB = new GeoCoordinate(locB.Location.Latitude, locB.Location.Longitude);
+
+                    var distancebetween = corA.GetDistanceTo(corB);
+
+                    if (distancebetween > distance)
+                    {
+                        distancebetween = distance;
+                        tacoBallOne = locA;
+                        tacoballTwo = locB;
+                    }
+                }
+            }
+
 
             // TODO: Once you have locA, create a new Coordinate object called `corA` with your locA's latitude and longitude.
 
@@ -54,19 +81,25 @@ namespace LoggingKata
             // TODO: Now, Inside the scope of your first loop, create another loop to iterate through locations again.
             // This allows you to pick a "destination" location for each "origin" location from the first loop. 
             // Naming suggestion for variable: `locB`
-
-            // TODO: Once you have locB, create a new Coordinate object called `corB` with your locB's latitude and longitude.
-
-            // TODO: Now, still being inside the scope of the second for loop, compare the two locations using `.GetDistanceTo()` method, which returns a double.
-            // If the distance is greater than the currently saved distance, update the distance variable and the two `ITrackable` variables you set above.
-
-            // NESTED LOOPS SECTION COMPLETE ---------------------
-
-            // Once you've looped through everything, you've found the two Taco Bells farthest away from each other.
-            // Display these two Taco Bell locations to the console.
-
-
-            
+            Console.WriteLine($"The farthest Taco Bell locations are:");
+            Console.WriteLine($"Location A: {tacoBallOne.Name} at ({tacoBallOne.Location.Latitude}, {tacoBallOne.Location.Longitude})");
+            Console.WriteLine($"Location B: {tacoballTwo.Name} at ({tacoballTwo.Location.Latitude}, {tacoballTwo.Location.Longitude})");
         }
+        // TODO: Once you have locB, create a new Coordinate object called `corB` with your locB's latitude and longitude.
+
+        // TODO: Now, still being inside the scope of the second for loop, compare the two locations using `.GetDistanceTo()` method, which returns a double.
+
+        // If the distance is greater than the currently saved distance, update the distance variable and the two `ITrackable` variables you set above.
+
+        // NESTED LOOPS SECTION COMPLETE ---------------------
+        
+        // Once you've looped through everything, you've found the two Taco Bells farthest away from each other.
+        // Display these two Taco Bell locations to the console.
+
+
+
     }
-}
+}   
+
+        
+
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -17,6 +17,7 @@
             // If your array's Length is less than 3, something went wrong
             if (cells.Length < 3)
             {
+                logger.LogInfo("something went wrong.");
                 // Log error message and return null
                 return null; 
             }
@@ -24,29 +25,35 @@
             // TODO: Grab the latitude from your array at index 0
             // You're going to need to parse your string as a `double`
             // which is similar to parsing a string as an `int`
-            
-            
+            var latitude = double.Parse(cells[0]);
+
             // TODO: Grab the longitude from your array at index 1
             // You're going to need to parse your string as a `double`
             // which is similar to parsing a string as an `int`
-            
-            
+            var longitude = double.Parse(cells[1]);
+
             // TODO: Grab the name from your array at index 2
-            
+            var name = cells[2];
+
 
             // TODO: Create a TacoBell class
             // that conforms to ITrackable
-            
+
             // TODO: Create an instance of the Point Struct
             // TODO: Set the values of the point correctly (Latitude and Longitude) 
-
+            var point = new Point();
+            point.Latitude = latitude;
+            point.Longitude = longitude;
             // TODO: Create an instance of the TacoBell class
             // TODO: Set the values of the class correctly (Name and Location)
+            var tacoball = new TacoBell();
+            tacoball.Name = name;
+            tacoball.Location = point;
 
             // TODO: Then, return the instance of your TacoBell class,
             // since it conforms to ITrackable
 
-            return null;
+            return tacoball;
         }
     }
 }
